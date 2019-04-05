41ef03611b9afe47d9d5ec7a4d8613319a5df8c7
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-04-05 16:31:52 +0100

    adding wpf and nodeview extension methods

---
+++ src/DiagnosticToolkit/src/Utilities/NodeViewModifier.cs
@@ -0,0 +1,21 @@
+﻿using Dynamo.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DiagnosticToolkit.Utilities
+{
+    public static class NodeViewModifier
+    {
+        public static NodeView ChangeBackground(this NodeView nodeView, int r, int g, int b)
+        {
+            Rectangle background = ((Rectangle)nodeView.grid.FindName("nodeBackground"));
+            background.Fill = new SolidColorBrush(Color.FromArgb(100, Convert.ToByte(r), Convert.ToByte(g), Convert.ToByte(b)));
+            return nodeView;
+        }
+    }
+}
