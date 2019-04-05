4475bb28db3e0226d64d77da5ec00028cff0af23
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-04-05 11:29:09 +0100

    view extension setup

---
+++ src/DiagnosticToolkit/src/DiagnosticToolkitWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DiagnosticToolkit
+{
+    /// <summary>
+    /// Interaction logic for DiagnosticToolkitWindow.xaml
+    /// </summary>
+    public partial class DiagnosticToolkitWindow : Window
+    {
+        public DiagnosticToolkitWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
