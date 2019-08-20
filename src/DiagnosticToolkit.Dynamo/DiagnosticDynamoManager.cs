a225c2105dacf711f9fb3e9ba8d73463943e264c
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-08-20 10:47:12 +0100

    new begginings

---
+++ src/DiagnosticToolkit.Dynamo/DiagnosticDynamoManager.cs
@@ -0,0 +1,65 @@
+﻿using Dynamo.ViewModels;
+using Dynamo.Wpf.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DiagnosticToolkit.Dynamo
+{
+    public class DiagnosticDynamoManager : IDisposable
+    {
+        private readonly Version MINIMUM_VERSION = new Version("2.3.0.5885");
+        private ViewLoadedParams loadedParameters { get; set; }
+        private DynamoViewModel dynamoVM { get; set; }
+        private Version dynamoVersion { get; set; }
+        private MenuItem mainMenu { get; set; }
+
+        public bool CanUseDiagnostic => dynamoVersion >= MINIMUM_VERSION;
+
+        public DiagnosticDynamoManager(ViewLoadedParams parameters)
+        {
+            this.loadedParameters = parameters;
+            this.dynamoVM = parameters.DynamoWindow.DataContext as DynamoViewModel;
+            this.dynamoVersion = parameters.StartupParams.DynamoVersion;
+
+            this.InitializeMenu();
+        }
+
+        public void InitializeMenu()
+        {
+
+            this.mainMenu = new MenuItem()
+            {
+                Header = "Diagnostic Toolkic"
+            };
+
+            var launchToolkit = new MenuItem()
+            {
+                Header = "Launch",
+            };
+
+            launchToolkit.Click += (sender, args) =>
+            {
+                if (!this.CanUseDiagnostic)
+                {
+                    MessageBox.Show($"The Diagnostic Toolkit cannot be used in your current Dynamo Version {dynamoVersion.ToString()}. Minimum version required is {MINIMUM_VERSION.ToString()}.");
+                    return;
+                }
+
+                MessageBox.Show("Can use disgnostic");
+            };
+
+            this.mainMenu.Items.Add(launchToolkit);
+            this.loadedParameters.dynamoMenu.Items.Add(this.mainMenu);
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ src/DiagnosticToolkit.Dynamo/DiagnosticViewExtension.cs
@@ -0,0 +1,33 @@
+﻿using Dynamo.Wpf.Extensions;
+using System;
+using System.Windows;
+
+namespace DiagnosticToolkit.Dynamo
+{
+    public class DiagnosticViewExtension : IViewExtension
+    {
+        public string UniqueId => "EF1D3B3E-3991-46BE-B75F-8429A49AE58C";
+        public string Name => "Diagnostic Toolkit for Dynamo";
+
+        private DiagnosticDynamoManager manager { get; set; }
+
+        public void Startup(ViewStartupParams parameters)
+        {
+        }
+
+        public void Loaded(ViewLoadedParams parameters)
+        {
+            this.manager = new DiagnosticDynamoManager(parameters);
+        }
+
+        public void Shutdown()
+        {
+           // throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            manager.Dispose();
+        }
+    }
+}
