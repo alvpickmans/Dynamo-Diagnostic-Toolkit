0692e8b69ab4678c252b7922c500f6802cb37323
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-08-22 19:10:40 +0100

    adding `IProfilingSession` interface

---
+++ src/DiagnosticToolkit.Core/Interfaces/IProfilingSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiagnosticToolkit.Core.Interfaces
+{
+    public interface IProfilingSession
+    {
+        TimeSpan ExecutionTime { get; }
+
+        event EventHandler SessionStarted;
+        event EventHandler SessionEnded;
+    }
+}
