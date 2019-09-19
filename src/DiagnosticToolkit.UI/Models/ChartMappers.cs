0b4579567a27dac35ecbbb1a3bf2e5267094cc3f
Alvaro Ortega Pickmans <aortegapickmans@gmail.com>
2019-09-19 13:47:04 +0200

    adds animated stroked to selected nodes

--- src/DiagnosticToolkit.UI/Models/ChartMappers.cs
+++ src/DiagnosticToolkit.UI/Models/ChartMappers.cs
@@ -12,15 +12,34 @@ namespace DiagnosticToolkit.UI.Models
 {
     public static class ChartMappers
     {
-        private static Brush NotExecutedBrush = new SolidColorBrush(Color.FromArgb(175, 175, 175, 175));
-        private static Brush ExecutionRequestedBrush = new SolidColorBrush(Color.FromArgb(175, 255, 165, 0));
-        private static Brush SelectedBrush = new SolidColorBrush(Color.FromArgb(175, 255, 0, 0));
+        private static Brush NotExecutedBrush = new SolidColorBrush(Color.FromArgb(240, 175, 175, 175));
+        private static Brush ExecutionRequestedBrush = new SolidColorBrush(Color.FromArgb(240, 255, 165, 0));
+        private static RadialGradientBrush SelectedBrush = new RadialGradientBrush(Color.FromArgb(0, 255, 255, 255), Color.FromArgb(240, 255, 255, 255));
+
+
+        static ChartMappers()
+        {
+            SelectedBrush.GradientStops.First().BeginAnimation(GradientStop.ColorProperty, RadialGradiantColorAnimation(SelectedBrush, false));
+            SelectedBrush.GradientStops.Last().BeginAnimation(GradientStop.ColorProperty, RadialGradiantColorAnimation(SelectedBrush, true));
+        }
+
+        private static ColorAnimation RadialGradiantColorAnimation(RadialGradientBrush brush, bool reverse)
+        {
+            var firstColor = brush.GradientStops.First().Color;
+            var lastColor = brush.GradientStops.Last().Color;
+
+            return new ColorAnimation()
+            {
+                From = reverse ? lastColor : firstColor,
+                To = reverse ? firstColor : lastColor,
+                Duration = new System.Windows.Duration(TimeSpan.FromSeconds(1)),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+        }
 
         private static Brush GetFillBrush(this ProfilingDataPoint point)
         {
-            if (point.Selected)
-                return SelectedBrush;
-
             if (point.Instance.HasExecutionPending)
                 return ExecutionRequestedBrush;
 
@@ -32,7 +51,7 @@ namespace DiagnosticToolkit.UI.Models
 
         private static Brush GetStrokeBrush(this ProfilingDataPoint point)
         {
-            return point.Instance.HasExecutionPending ? ExecutionRequestedBrush : point.GetFillBrush();
+            return !point.Selected ? Brushes.Transparent : (Brush)SelectedBrush;
         }
 
         public static WeightedMapper<ProfilingDataPoint> ProfilingDataPointMapper = Mappers
@@ -40,6 +59,7 @@ namespace DiagnosticToolkit.UI.Models
             .X(value => value.X)
             .Y(value => value.Y)
             .Weight(value => value.Weight)
-            .Fill(value => value.GetFillBrush());
+            .Fill(value => value.GetFillBrush())
+            .Stroke(value => value.GetStrokeBrush());
     }
 }
